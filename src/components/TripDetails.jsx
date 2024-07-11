a3e1c2f5896df0db5d565d4b55ed3a0df9c17aa3
Medix13 <mahdi.pk98@gmail.com>
2024-07-11 07:30:46 +0200

    update

--- src/components/TripDetails.jsx
+++ src/components/TripDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { fetchTrip, deleteMeeting, addMeeting } from '../services/tripService';
 import { TrashIcon } from '@heroicons/react/outline';
 
